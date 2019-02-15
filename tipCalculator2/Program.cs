85243bf3dcb3706950b3b34483d501c5a021488c
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-15 16:59:42 -0600

    changed

--- tipCalculator2/Program.cs
+++ tipCalculator2/Program.cs
@@ -6,7 +6,38 @@ namespace tipCalculator2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the total of your bill?");
+
+            string totalBillInput = Console.ReadLine();
+            totalBillInput = totalBillInput.Trim().Trim('$');
+
+            Console.WriteLine("What percentage do you want to tip?");
+
+            string tipPercentageInput = Console.ReadLine();
+            tipPercentageInput = tipPercentageInput.Trim().Trim('%');
+
+            //decimal totalBill = decimal.Parse(totalBillInput);
+            decimal tipPercentage;
+            //int tipPercentage = int.Parse(tipPercentageInput);
+            if (!decimal.TryParse(tipPercentageInput, out tipPercentage))
+            {
+                Console.WriteLine($"{tipPercentageInput} is an invalid value");
+
+            }
+            decimal totalBill;
+             if (!decimal.TryParse(totalBillInput, out totalBill))
+            {
+                Console.WriteLine($"{totalBillInput} is an invalid value");
+
+            }
+            decimal tipAmount = tipPercentage * totalBill / 100;
+            tipAmount = Math.Round(tipAmount, 2);
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }

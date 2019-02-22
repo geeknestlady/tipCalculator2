e8d83b3ce6796238df210357004581ca37164529
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-22 14:15:56 -0600

    Finished by writing out losers' names.

--- RandomNamePrinter/Program.cs
+++ RandomNamePrinter/Program.cs
@@ -22,15 +22,28 @@ namespace RandomNamePrinter
                 }
                 names[i] = name;
             }
-            int randomInt = new Random().Next(0, numberOfNames -1);
+            int randomInt = new Random().Next(0, numberOfNames);
             string nameWinner = names[randomInt];
             
             Console.WriteLine($"The winner is: {nameWinner}");
+            Console.WriteLine("The losers are:");
+            for (int i = 0; i < names.Length; i++)
+            {
+                string name = names[i];
+                if(names[i] != nameWinner)
+                {
+                    names[i] = name;
+                    Console.WriteLine(name);
+                }
+            }
+            Console.ReadLine();
 
-         
-            
 
-            //store the name in an arrary
+        }
+
+           // string namesList = names.ToList();
+                                             
+             //store the name in an arrary
             //repeat this for up to 30 names
             //not: if the user hits enter w/o a name, assume they are done and stop asking them
             //get a random number between 0 and the number of names entered -1
@@ -39,7 +52,7 @@ namespace RandomNamePrinter
             //print list of the other names that did not win
             //pause with readline
 
-            Console.ReadLine();
+            
         }
     }
-}
+
